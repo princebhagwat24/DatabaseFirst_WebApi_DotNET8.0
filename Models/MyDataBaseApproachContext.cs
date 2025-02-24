9f9c23e93319a4ba0de99e7edc5461b65b9e2e73
Prince Bhagwat <104710793+princebhagwat24@users.noreply.github.com>
2025-02-24 14:03:14 +0530

    Add files via upload

---
+++ Models/MyDataBaseApproachContext.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DatabaseFirstWebApi.Models
+{
+    public class Table1
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)] 
+        public string Name { get; set; }
+
+        public int Value { get; set; } 
+    }
+
+    public class MyDataBaseApproachContext : DbContext
+    {
+        public MyDataBaseApproachContext(DbContextOptions<MyDataBaseApproachContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Table1> Table1 { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Table1>()
+                .ToTable("Table1"); 
+        }
+    }
+}
