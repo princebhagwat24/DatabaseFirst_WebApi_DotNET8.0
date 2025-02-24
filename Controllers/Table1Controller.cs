16561614e9aca528ee0d51973a0a64b753bb161c
Prince Bhagwat <104710793+princebhagwat24@users.noreply.github.com>
2025-02-24 14:02:03 +0530

    Add files via upload

---
+++ Controllers/Table1Controller.cs
@@ -0,0 +1,27 @@
+﻿using DatabaseFirstWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatabaseFirstWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Table1Controller : ControllerBase
+    {
+        private readonly MyDataBaseApproachContext _context;
+
+        public Table1Controller(MyDataBaseApproachContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddTableData(Table1 data)
+        {
+            _context.Table1.Add(data);
+            await _context.SaveChangesAsync();
+
+            return Ok(data);
+        }
+
+    }
+}
